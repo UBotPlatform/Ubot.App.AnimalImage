456cf24af3939311f2fd16e3442a27f6fe26957e
qiqiworld <1354092549@qq.com>
2020-08-27 23:52:48 +0800

    init: upload local files

---
+++ README.md
@@ -0,0 +1,23 @@
+## About UBot
+UBot is an open source platform for developing chat bots. It provides consistent apis for different platforms (telegram, discord and more!) in a language-independent way, making bot development WORA-able (Write once, Run anywhere).
+
+## About AnimalImage
+AnimalImage is an App on UBot Platform. It provides commands for requiring random animal images. All the images are from [Some Random Api](https://some-random-api.ml/)
+
+## Commands
+```json
+[
+    {"keywords": ["/cat", "吸猫"], "animal": "cat"},
+    {"keywords": ["/dog", "狗狗"], "animal": "dog"},
+    {"keywords": ["/red_panda", "小熊猫", "红熊猫", "小猫熊", "红猫熊", "金狗"], "animal": "red_panda"},
+    {"keywords": ["/panda", "熊猫", "猫熊"], "animal": "panda"},
+    {"keywords": ["/bird", "小鸟", "鸟图"], "animal": "birb"},
+    {"keywords": ["/fox", "狐狸"], "animal": "fox"},
+    {"keywords": ["/koala", "考拉", "无尾熊", "可拉熊", "树懒熊"], "animal": "koala"}
+]
+```
+
+## License
+This application is licensed under BSD 3-Clause License.  
+Please see [LICENSE](LICENSE.md) for licensing details.  
+You can use TLDRLegal to see a [summary](https://www.tldrlegal.com/l/bsd3) first. (***!!!NOT LEGAL ADVICE!!!***)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	ubot "github.com/UBotPlatform/UBot.Common.Go"
+)
+
+var api *ubot.AppApi
+
+type AnimalImageResponse struct {
+	Link string `json:"link"`
+}
+type AnimalNameMapping struct {
+	keywords   []string
+	animalType string
+}
+
+var mappings = [...]AnimalNameMapping{
+	{keywords: []string{"/cat", "吸猫"}, animalType: "cat"},
+	{keywords: []string{"/dog", "狗狗"}, animalType: "dog"},
+	{keywords: []string{"/red_panda", "小熊猫", "红熊猫", "小猫熊", "红猫熊", "金狗"}, animalType: "red_panda"},
+	{keywords: []string{"/panda", "熊猫", "猫熊"}, animalType: "panda"},
+	{keywords: []string{"/bird", "小鸟", "鸟图"}, animalType: "birb"},
+	{keywords: []string{"/fox", "狐狸"}, animalType: "fox"},
+	{keywords: []string{"/koala", "考拉", "无尾熊", "可拉熊", "树懒熊"}, animalType: "koala"},
+}
+
+var cachedAnimalImages = make(map[string][]string)
+
+func onReceiveChatMessage(bot string, msgType ubot.MsgType, source string, sender string, message string, info ubot.MsgInfo) (ubot.EventResultType, error) {
+	animalType := ""
+mappingLoop:
+	for _, mapping := range mappings {
+		for _, keyword := range mapping.keywords {
+			if strings.Contains(message, keyword) {
+				animalType = mapping.animalType
+				break mappingLoop
+			}
+		}
+	}
+	if animalType == "" {
+		return ubot.IgnoreEvent, nil
+	}
+	for {
+		var builder ubot.MsgBuilder
+		resp, err := http.Get(fmt.Sprintf("https://some-random-api.ml/img/%s?t=%d", animalType, time.Now().Unix()))
+		if err != nil {
+			break
+		}
+		defer resp.Body.Close()
+		var result AnimalImageResponse
+		err = json.NewDecoder(resp.Body).Decode(&result)
+		if err != nil {
+			cachedList := cachedAnimalImages[animalType]
+			if len(cachedList) == 0 {
+				_ = api.SendChatMessage(bot, msgType, source, sender, "请求图片失败，可能是网络问题或频率限制")
+				break
+			}
+			result.Link = cachedList[time.Now().Unix()%int64(len(cachedList))]
+		} else {
+			if len(cachedAnimalImages[animalType]) >= 100 {
+				cachedAnimalImages[animalType][time.Now().Unix()%100] = result.Link
+			} else {
+				cachedAnimalImages[animalType] = append(cachedAnimalImages[animalType], result.Link)
+			}
+		}
+		builder.WriteEntity(ubot.MsgEntity{Type: "image_online", Data: result.Link})
+		_ = api.SendChatMessage(bot, msgType, source, sender, builder.String())
+		break //nolint
+	}
+	return ubot.CompleteEvent, nil
+}
+
+func main() {
+	err := ubot.HostApp("AnimalImage", func(e *ubot.AppApi) *ubot.App {
+		api = e
+		return &ubot.App{
+			OnReceiveChatMessage: onReceiveChatMessage,
+		}
+	})
+	ubot.AssertNoError(err)
+}
